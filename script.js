a8df52006d7a30dedbf47f39d70b0145a029f431
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-05-17 12:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
-//complete this code
-// Rectangle class
 class Rectangle {
-  constructor(width, height) {
-    this._width = width;
-    this._height = height;
-  }
-
-  // Getter for width
-  get width() {
-    return this._width;
-  }
-
-  // Getter for height
-  get height() {
-    return this._height;
-  }
-
-  // Method to calculate area
-  getArea() {
-    return this._width * this._height;
-  }
+constructor(width, height) {
+this._width = width;
+this._height = height;
 }
 
-// Square subclass
+get width() {
+return this._width;
+}
+
+get height() {
+return this._height;
+}
+
+getArea() {
+return this._width * this._height;
+}
+}
+
 class Square extends Rectangle {
-  // constructor(sideLength) {
-  //   super(sideLength, sideLength);
-  // }
-
-  // Method to calculate perimeter
-  getPerimeter() {
-    return 4 * this.width;
-  }
+constructor(side) {
+super(side, side);
 }
- 
-// Example usage
-// const rectangle = new Rectangle(5, 10);
-// console.log(rectangle.width); // Output: 5
-// console.log(rectangle.height); // Output: 10
-// console.log(rectangle.getArea()); // Output: 50
 
-// const square = new Square(7);
-// console.log(square.width); // Output: 7
-// console.log(square.height); // Output: 7
-// console.log(square.getArea()); // Output: 49
-// console.log(square.getPerimeter()); // Output: 28
+getPerimeter() {
+return 4 * this.width;
+}
+}
 
+const rect = new Rectangle(5, 10);
+console.log(rect.width); // output: 5
+console.log(rect.height); // output: 10
+console.log(rect.getArea()); // output: 50
+
+const square = new Square(7);
+console.log(square.width); // output: 7
+console.log(square.height); // output: 7
+console.log(square.getArea()); // output: 49
+console.log(square.getPerimeter()); // output: 28
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
